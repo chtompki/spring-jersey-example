56f6c097e522ddc08fc27e06912d9295f1989b7d
Rob Tompkins <chtompki@gmail.com>
2015-11-04 18:24:53 -0800

    Adding slf4j logging implementation

--- src/main/java/com/rt/resource/ExampleResource.java
+++ src/main/java/com/rt/resource/ExampleResource.java
@@ -1,5 +1,7 @@
 package com.rt.resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -7,7 +9,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.logging.Logger;
 
 /**
  * Created by tompkicr on 11/4/15.
@@ -18,19 +19,22 @@ import java.util.logging.Logger;
 public class ExampleResource {
 
     private static final String CLASSNAME = ExampleResource.class.getName();
-    private static final Logger LOGGER = Logger.getLogger(CLASSNAME);
+    private Logger LOGGER = LoggerFactory.getLogger(CLASSNAME);
 
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/v1_0")
     public String getJsonExampleString() {
+        String METHOD_NAME="getJsonExampleString";
+        LOGGER.info("Entered method: {}", METHOD_NAME);
         String exampleResponse;
         try {
             exampleResponse = JSON_EXAMPLE_STRING;
         } catch (Exception e) {
             exampleResponse = "failure";
         }
+        LOGGER.info("Exiting method: {}", METHOD_NAME);
         return exampleResponse;
     }
 
